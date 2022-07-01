903a0289421113e4590ae99daf4561a55cb3f7ed
gopi <32951153+gopimandala@users.noreply.github.com>
2022-07-01 10:46:37 +0530

    Update 07-install-chaosctl.sh

--- 07-install-chaosctl.sh
+++ 07-install-chaosctl.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+curl -O https://chaosctl.chaosnative.com/chaosctl-linux-amd64-v0.2.0.tar.gz && 
 tar -zxvf chaosctl-linux-amd64-v0.2.0.tar.gz && chmod +x chaosctl &&
 sudo mv chaosctl /usr/local/bin/chaosctl && 
 chaosctl version
